b8b4567bf2450a8a02a0201e6de986423d35e7d8
nobilis <nikolaev-evgeny@yandex.ru>
2022-05-30 23:59:23 +0300

    2022-05-30/23.59 AccessoryRepository. Удаление избыточных аннотаций @Query.

--- main/src/java/computerAccessories/repository/AccessoryRepository.java
+++ main/src/java/computerAccessories/repository/AccessoryRepository.java
@@ -9,10 +9,8 @@ import java.util.Optional;
 
 public interface AccessoryRepository extends JpaRepository<Accessory, Long> {
 
-    @Query("select a from Accessory a where a.modelId = :modelId")
     Optional<Accessory> findByModelId(String modelId);
 
-    @Query("select a from Accessory a where a.type = :type")
     Page<Accessory> findAllByType(Pageable pageable, String type);
 
     @Query("select a from Accessory a where a.type = :type and a.code = :vendor")
